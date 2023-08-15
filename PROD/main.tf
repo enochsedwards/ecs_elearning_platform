c4e20c6b23fd44530918bb83a76a522aec372923
Patrick Affadu <kaffadu@Patricks-MacBook-Pro-3.local>
2023-08-15 14:26:18 +0100

    statelock removed update

--- PROD/main.tf
+++ PROD/main.tf
@@ -1,4 +1,4 @@
-terraform {
+/*terraform {
   backend "s3" {
     bucket         = "skillsedgelab-s3statebackend"
     dynamodb_table = "sel-e-learning-dynamodbtable"
@@ -7,7 +7,7 @@ terraform {
     encrypt        = true
   }
 }
-
+*/
 
 
 # Configure the AWS Provider
